db28ff5069bdba8c230f71c4e8bc2f269901a326
Riyor <1014728780@qq.com>
2023-01-11 11:45:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author Riyor
+ * @data 2023/01/11 11:38
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
